a0f94b90cd0a58e57ba6ba06b9ac7287ce5dfedb
Cocoricodai <46318872+cocoricodai@users.noreply.github.com>
2021-05-22 23:26:31 +0200

    Delete Projet Web - final - mobile directory

--- Projet Web - final - mobile/main.js
+++
@@ -1,8 +0,0 @@
-function Hamburger() {
-    var x = document.getElementById("nav_hamburger");
-    if (x.style.top === "0px") {
-        x.style.top = "-300px";
-    } else {
-        x.style.top = "0px";
-    }
-}
